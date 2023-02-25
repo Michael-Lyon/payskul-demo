f8a6664b624a0bd68c535931b240192ea49c64e1
Michael Lyon <michaellyon@localhost.localdomain>
2023-02-25 13:37:23 +0100

    Corrected list user link

--- account/urls_v1.py
+++ account/urls_v1.py
@@ -5,7 +5,7 @@ app_name = "account"
 
 urlpatterns = [
     path('create-user/', views_v1.create_user, name="create_user"),
-    path('list-user', views_v1.UserListView.as_view(), name="list_create_user"),
+    path('list-user', views_v1.UserListView.as_view(), name="list_user"),
     path('email-verify/', views_v1.confirm_email, name="confirm_email"),
     path('login/', views_v1.LoginView.as_view(), name='login'),
 ]
