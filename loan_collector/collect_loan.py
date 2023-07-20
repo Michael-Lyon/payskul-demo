7f7e3f49d5e25e4915e67c8e9be1ab74c790b9b6
michaellyon <mickiasomg@gmail.com>
2023-07-20 14:11:24 +0100

    Updated Loan Collector

--- loan_collector/collect_loan.py
+++ loan_collector/collect_loan.py
@@ -21,9 +21,13 @@ def get_expired_loans():
     one_month_ago = get_current_date() - timedelta(days=30)
 
     # Use Q objects to construct the filter
-    expired_loans = Loan.objects.filter(Q(end_date__lt=one_month_ago) | Q(end_date__isnull=True))
+    try:
+        expired_loans = Loan.objects.filter(Q(end_date__lt=one_month_ago) | Q(end_date__isnull=True))
 
-    return expired_loans
+        return expired_loans
+    except Exception as e:
+        print(e)
+        return None
 
 
 def perform_loan_repayment(loan, charge, user, api_reference, api_status):
@@ -61,47 +65,48 @@ def check_expired_loans():
     paystack_banks = Okra.paystack_banks()
 
     # Iterate over expired loans
-    for loan in expired_loans:
-        # Get the user of the loan
-        user = loan.user
+    if expired_loans:
+        for loan in expired_loans:
+            # Get the user of the loan
+            user = loan.user
 
-        # Check if the user has an account (nuban)
-        okra_link = get_object_or_404(OkraLinkedUser, user=user)
-        charge = (okra_link.avg_income * PAYMENT_RATE) # amount to charge the user
+            # Check if the user has an account (nuban)
+            okra_link = get_object_or_404(OkraLinkedUser, user=user)
+            charge = (okra_link.avg_income * PAYMENT_RATE) # amount to charge the user
 
-        if not okra_link.income_accounts:
-            if okra.get_nuban_balances(okra_link.customer_id):
-                # CHECK BALANCE IN EACH ACCOUNT 
-                balances = okra_link.balance_ids.split(":") # THE ":" is a delimeter for all balance ids
-                nubans = okra_link.income_accounts.split(":") # THE ":" is a delimeter for
-                banks = okra_link.income_banks.split(":")
-                # LOOP THROUGH EACH BALANCE TO MAKE A CHARGE ON ACCOUNT WITH SUFFICIENT BALANCE
-                for i in range(len(balances)):
-                    nuban = nubans[i]
-                    balance = balances[i] 
-                    bank = banks[i]
+            if not okra_link.income_accounts:
+                if okra.get_nuban_balances(okra_link.customer_id):
+                    # CHECK BALANCE IN EACH ACCOUNT 
+                    balances = okra_link.balance_ids.split(":") # THE ":" is a delimeter for all balance ids
+                    nubans = okra_link.income_accounts.split(":") # THE ":" is a delimeter for
+                    banks = okra_link.income_banks.split(":")
+                    # LOOP THROUGH EACH BALANCE TO MAKE A CHARGE ON ACCOUNT WITH SUFFICIENT BALANCE
+                    for i in range(len(balances)):
+                        nuban = nubans[i]
+                        balance = balances[i] 
+                        bank = banks[i]
 
-                    # Check for the corresponding bank in Paystack Api
-                    bank_details = next((p_bank for p_bank in paystack_banks if p_bank['name'] == bank), None)
+                        # Check for the corresponding bank in Paystack Api
+                        bank_details = next((p_bank for p_bank in paystack_banks if p_bank['name'] == bank), None)
 
-                    if bank_details:        
-                        bal = okra.get_balance(balance_id=balance)
-                        if bal >= charge:
-                            api_data = okra._initiate_payment(nuban, okra_link.user, bank_details['code'], charge)
-                
-                            # Check if API call was successful
-                            if api_data:
-                                # Parse API response and extract necessary information
-                                api_reference = api_data.get("ref")
-                                api_status = api_data.get("status")
+                        if bank_details:        
+                            bal = okra.get_balance(balance_id=balance)
+                            if bal >= charge:
+                                api_data = okra._initiate_payment(nuban, okra_link.user, bank_details['code'], charge)
+                    
+                                # Check if API call was successful
+                                if api_data:
+                                    # Parse API response and extract necessary information
+                                    api_reference = api_data.get("ref")
+                                    api_status = api_data.get("status")
 
-                                # Perform loan repayment if API call was successful
-                                perform_loan_repayment(loan, charge, user, api_reference, api_status)
+                                    # Perform loan repayment if API call was successful
+                                    perform_loan_repayment(loan, charge, user, api_reference, api_status)
+                                else:
+                                    logger.error("API call for loan repayment failed.")
                             else:
-                                logger.error("API call for loan repayment failed.")
+                                logger.warning("Insufficient balance for loan repayment.")
                         else:
-                            logger.warning("Insufficient balance for loan repayment.")
-                    else:
-                        logger.error("COLLECT LOAN: No Bank Match found for", user)
-            else:
-                logger.warning("No nuban balances found for user", user)
+                            logger.error("COLLECT LOAN: No Bank Match found for", user)
+                else:
+                    logger.warning("No nuban balances found for user", user)
